4b985597ef1de271cf06161aec756f7ec5894b6b
John  Owusu Duah <johnowusuduah@Johns-MacBook-Pro.local>
2021-09-16 14:53:04 -0400

    Plot of Mortality vs GDP added

---
+++ gdpscript.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+data = pd.read_csv(
+    "https://media.githubusercontent.com/media/nickeubank/MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv"
+)
+
+# data.head()
+
+data1 = data.loc[
+    :,
+    [
+        "Mortality rate, infant (per 1,000 live births)",
+        "GDP per capita (constant 2010 US$)",
+        "Country Name",
+    ],
+]
+
+
+plt.scatter(
+    data=data1,
+    x="GDP per capita (constant 2010 US$)",
+    y="Mortality rate, infant (per 1,000 live births)",
+)
+plt.show()
